f19cd25d369089f0951453b9785a97eb828f08c2
Thanuja Priyadarshane <thanujapriyadarshane@Thanujas-MacBook-Air.local>
2024-06-20 11:08:31 +0530

    changes

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const user = require('./routers/user');
+const student = require('./routers/student');
+const auth = require('./middleware/auth');
+require('dotenv').config();
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/api/user',user);
+app.use('/api/student',auth,student) ;
+
+
+
+const port = process.env.PORT || 3000;
+
+app.listen(port,()=>console.log(`App is running on port ${port}`));
